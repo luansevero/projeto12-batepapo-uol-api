cffa1a488f8648e6200eb1c8200cdb29152939f5
Luan Severo <luansevero@icloud.com>
2022-06-27 08:45:36 -0300

    HotFix in Login

--- src/api/participante.js
+++ src/api/participante.js
@@ -1,23 +1,25 @@
 import dayjs from "dayjs";
+import { strict as assert } from 'assert';
+import { stripHtml } from "string-strip-html";
 import mongoClient from '../database/mongo.js';
 import { participantValidation } from '../verification/JoiVerifications.js'
 
 
 async function login(req,res){
-    const user = {...req.body, lastStatus: Date.now()};
-    if(!participantValidation(user)){return res.sendStatus(422)};
+    const participant = {...req.body, lastStatus: Date.now()};
+    if(!participantValidation(participant)){return res.sendStatus(422)};
 
     await mongoClient.connect();
     const db = mongoClient.db('bate_papo_uol');
 
     const participantCollection = db.collection('participante');
     
-    const isLoggedIn = await participantCollection.findOne({name: user});
+    const isLoggedIn = await participantCollection.findOne({name: participant.name});
     if(isLoggedIn){return res.sendStatus(409)};
     try{
-        await participantCollection.insertOne(user);
+        await participantCollection.insertOne(participant);
         await db.collection('mensagens').insertOne({
-            from: user.name,
+            from: participant.name,
             to: 'Todos',
             text: 'entra na sala...',
             type: 'status',
